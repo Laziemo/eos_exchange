d7c66f3f74ef0213c39a9f6793312b7ccba692c2
EOS <vishalmenon.92@gmail.com>
2019-01-09 20:05:02 +0530

    Receive monitor functionality. eosjs still broken

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # eos_exchange
 
-## interface.js
+## index.js
 
 Uses eosjs to sign transactions.
+
+//Can be extended to deprecate interface.js using JSONRpc class
 
 ## interface.js
 
@@ -13,7 +15,6 @@ Makes API calls to an EOS node for the following methods:
 /get_account {"account_name":"stestnettbit"}
 
 /get_actions {"pos":-1,  "offset":1, "account_name":"stestnettbit"}
-
 
 CURL TESTS:
 
@@ -28,10 +29,34 @@ curl -X POST "http://127.0.0.1:8877/get_account"
 
 *Environment Variables*
 
-export NODE=<port defined in config.ini::http-server-address>
+export NODE=*<port defined in config.ini::http-server-address>*
 
 
 Requires call to: 
 
->push_transaction(s)
+/push_transaction(s)
 
+##monitor.js
+
+Cron tab makes get_actions rpc calls every 5 seconds and calles update.js with a set of new receives.
+
+
+Overwrites tip_backup.bloc with the latest transaction block_num to be used as a reference for filtering new receives.
+
+
+## update.js
+
+
+Listens for requests from monitor.js to update a local mongodb.
+
+
+### REQUIRED UPGRADES:
+
+Use ampq messaging queue for more dynamic updates to client. 
+
+Use db to only store receives that have been validated by the client as updated.
+
+Store latest block_num (tip_block) accordingly.
+
+
+## Support/Bug Reporting: viz@throughbit.com
--- index.js
+++ index.js
@@ -17,75 +17,51 @@ const bodyParser = require('body-parser');
 //     sys: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f' // local developer
 //     };
 
-// eos = Eos({
-//     keyProvider: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',// private key
-//     httpEndpoint: 'http://127.0.0.1:8888',
-//     chainId: chain.jungle,
-//     });
 
-
-// const Eos = require('eosjs');
-// const eos = Eos({ 
-//     keyProvider: : '5Hqst2zn4MSfjMWocGGyfCNPZDaVszet7ZWjEEedgka1fNZJesL',
-//     httpEndpoint, 
-//     chainId, 
-//     RpcError, 
-//     JsSignatureProvider 
-// });
 
 const { Api, JsonRpc, RpcError } = require('eosjs');
-// const EosApi = require('eosjs-api');
 const fetch = require('node-fetch');
 const { TextEncoder, TextDecoder } = require('util');
-
-//const JsSignatureProvider = require('eosjs/dist/eosjs-jssig');
 const JsSignatureProvider = require('eosjs/dist/eosjs-jssig').default; 
 //-<.init.>===========================================================~|
 
-const signatureProvider =  new JsSignatureProvider(["5K6ZYo5Yk6vPgjFHwmXU3StRq2hV7bxWr431SCqUBeKAmnoP1K8"]);
-//eos = Eos({keyPrefix: 'PUB'})
- 
-// Read-only instance when 'eosjs' is already a dependency
-//eos = Eos.modules.api({/*config*/})
- 
-// Read-only instance when an application never needs to write (smaller library)
-//EosApi = require('eosjs-api')
-//eos = EosApi({/*config*/})
+const signatureProvider =  new JsSignatureProvider([process.env.APK]);
 
-
-const rpc = new JsonRpc('http://jungle2.cryptolions.io:80', { fetch });
+const rpc = new JsonRpc('http://127.0.0.1:8888', { fetch });
 const api = new Api({ rpc,signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
 //-<.f(x).>===========================================================~|
 // console.log("test");
-rpc.get_account('stestnettbit').then(console.log);
-
 //-<.f(x).>===========================================================~|
-try{
     (async () => {
-        const result = await api.transact({
-          actions: [{
-            account: 'eosio.token',
-            name: 'transfer',
-            authorization: [{
-              actor: 'stestnettbit',
-              permission: 'active',
-            }],
-            data: {
-              from: 'stestnettbit',
-              to: 'ptestnettbit',
-              quantity: '0.01 EOS',
-              memo: 'itsvishalplease',
-            },
-          }]
-        }, {
-          blocksBehind: 3,
-          expireSeconds: 30,
-        });
-        console.dir(result);
+        try{
+          const result = await api.transact({
+              actions: [{
+                account: 'eosio.token',
+                name: 'transfer',
+                authorization: [{
+                  actor: 'stestnettbit',
+                  permission: 'active',
+                }],
+              data: {
+                  from: 'stestnettbit',
+                  to: 'ptestnettbit',
+                  quantity: "1 EOS",
+                  memo: 'itsvishalplease',
+              },
+              }]
+          }, {
+            blocksBehind: 3,
+            expireSeconds: 30,
+          });
+          console.dir(JSON.stringify(result,null,2));
+        }
+            
+        catch(e){
+            if (e instanceof RpcError)
+                console.log(JSON.stringify(e.json.error.details,null,2));
+            else 
+                console.log(JSON.stringify(e,null,2));
+        }
+
       })();
-}
-catch(e){
-    if (e instanceof RpcError)
-        console.log(JSON.stringify(e.json,null,2));
-}
 //-<.fin.>============================================================~|
